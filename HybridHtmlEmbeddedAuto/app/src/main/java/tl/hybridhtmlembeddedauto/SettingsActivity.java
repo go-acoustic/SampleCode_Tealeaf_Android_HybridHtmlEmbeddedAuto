8407271ff45d45ffda5cabf37989c346418fb390
ChangJie Yang <changjie.yang@acoustic.co>
2023-03-09 17:09:59 -0800

    EAOC-14577 Updated build versions, and fixed App issue.

--- HybridHtmlEmbeddedAuto/app/src/main/java/tl/hybridhtmlembeddedauto/SettingsActivity.java
+++ HybridHtmlEmbeddedAuto/app/src/main/java/tl/hybridhtmlembeddedauto/SettingsActivity.java
@@ -19,8 +19,9 @@ public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
         final EditText postMessageUrl = findViewById(R.id.postMessageUrl);
